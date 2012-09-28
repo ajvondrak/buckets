fcd08ecf0f7eccd408f925b08e81ae438ed4af0e
Alex Vondrak <alex@alex-pc>
2012-09-28 15:20:44 -0700

    Initial commit

---
+++ buckets.py
@@ -0,0 +1,9 @@
+"""UCI Puzzle - Buckets
+
+You're out hiking with friends and everyone's getting hungry. You have a packet
+of soup mix, and its recipe requires exactly 4 quarts of water. Unfortunately,
+you forgot your 4-quart cooking pot at home. All you have is a 5-quart pot and
+a 3-quart pot. There is a stream nearby, so you have an unlimited source of
+water. How will you use the 5-quart pot and the 3-quart pot to measure exactly
+4 quarts of water?
+"""
